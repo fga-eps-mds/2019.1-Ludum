d41972c0b0402ee35146b056a346fb2b5296a118
jonjon667 <jonjon667@hotmail.com>
2019-04-10 22:55:25 -0300

    #36 Enviando mensagem de ajuda.
    Co-authored-by: jprcorreia <joaopedrocorreia9@gmail.com>

--- src/helloLudum.py
+++ src/helloLudum.py
@@ -23,8 +23,20 @@ inicio_string.append("Olá, eu sou o Ludum! :D\n" +
                      "Vou te mostrar tudo que sei sobre desenvolvimento de jogos em Python!\n"+
                      "Mas antes, jovem padawan, devo te fazer uma pergunta: Você já conhece minhas funcionalidades?")
 
+ajuda_string = []
+ajuda_string.append("1)Tutorial: Vamos construir um jogo juntos!"+
+                    " Vou te ensinar sobre o desenvolvimento de jogos em Python desde as partes mais básicas"+
+                    " e quando você perceber já estará jogando seu próprio jogo!\n"+
+                    "2)Ambiente: Irei te auxiliar a preparar todo seu ambiente de desenvolvimento com os programas adequados.\n"+
+                    "3)Ajuda: Tentarei sanar todas as duvidas que você pode vir a ter.\n"+
+                    "Começar com o que, devemos?"
+                )
 
 @ludumBot.message_handler(commands=['start'])
 def send_start(mensagem):
     ludumBot.send_message(mensagem.chat.id,inicio_string)
 
+@ludumBot.message_handler(commands=['help'])
+def send_help(mensagem):
+    ludumBot.send_message(mensagem.chat.id,ajuda_string)
+ludumBot.polling()
