c64696cb2bf38f31045a45f08596171ac46e8e88
Wictor Bastos Girardi <wictor.girardi@gmail.com>
2019-04-12 20:36:20 -0300

    #36 Corrigindo tolken e mensagem de inicio.
    Co-authored-by: jprcorreia <joaopedrocorreia9@gmail.com>
    Co-authored-by: jonjon667 <jonjon667@hotmail.com>
    Co-authored-by: lucasgandac <lucasgandac@hotmail.com>

--- src/helloLudum.py
+++ src/helloLudum.py
@@ -13,7 +13,7 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 #Criar o bot
-token = "805272362:AAGys__qy0fJNef-3hUT8reu8vIEnNHb3PA"
+token = open('tolken.txt', 'r').read()
 ludumBot = telebot.TeleBot(token)
 
 #Texto de inicio
@@ -23,20 +23,7 @@ inicio_string.append("Olá, eu sou o Ludum! :D\n" +
                      "Vou te mostrar tudo que sei sobre desenvolvimento de jogos em Python!\n"+
                      "Mas antes, jovem padawan, devo te fazer uma pergunta: Você já conhece minhas funcionalidades?")
 
-ajuda_string = []
-ajuda_string.append("1)Tutorial: Vamos construir um jogo juntos!"+
-                    " Vou te ensinar sobre o desenvolvimento de jogos em Python desde as partes mais básicas"+
-                    " e quando você perceber já estará jogando seu próprio jogo!\n"+
-                    "2)Ambiente: Irei te auxiliar a preparar todo seu ambiente de desenvolvimento com os programas adequados.\n"+
-                    "3)Ajuda: Tentarei sanar todas as duvidas que você pode vir a ter.\n"+
-                    "Começar com o que, devemos?"
-                )
-
 @ludumBot.message_handler(commands=['start'])
 def send_start(mensagem):
-    ludumBot.send_message(mensagem.chat.id,inicio_string)
-
-@ludumBot.message_handler(commands=['help'])
-def send_help(mensagem):
-    ludumBot.send_message(mensagem.chat.id,ajuda_string)
+    ludumBot.send_message(mensagem.chat.id,inicio_string,)
 ludumBot.polling()
