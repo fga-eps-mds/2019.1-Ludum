195ea5528d840f3dc9798eced35d9cf3fa5da707
jonjon667 <jonjon667@hotmail.com>
2019-05-08 21:24:23 -0300

    #107 - Modificando request para recuperar somente os titulos das perguntas
    Co-authored-by: Wictorgirardi <wictor.girardi@gmail.com>

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -16,7 +16,7 @@ class ActionTest(Action):
         try:
             link = requests.get('https://ludum-duvidas.herokuapp.com/api/duvidas')
             stack = json.loads(link.text)
-            dispatcher.utter_message(str(stack['data']))
+            dispatcher.utter_message(str(stack['data'][0]['answer'][0]['_id']))
         except ValueError:
             dispatcher.utter_message(ValueError)
         return []
@@ -34,8 +34,7 @@ class ActionQuestion(Action):
         try:
             link = requests.get('https://ludum-duvidas.herokuapp.com/api/duvidas')
             stack = json.loads(link.text)
-            stackObjeto = json.dumps(stack)
-            dispatcher.utter_message(str(stack['data'][0]))
+            dispatcher.utter_message(str(stack['data'][0]['answer'][0]['title']))
         except ValueError:
             dispatcher.utter_message(ValueError)
         return []
