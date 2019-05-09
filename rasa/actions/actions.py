6a6c4ae42dca70573667e3651cbbeb6d91b1a3d7
jonjon667 <jonjon667@hotmail.com>
2019-05-09 18:12:29 -0300

    #107 - Adequando o código a folha de estilo

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -16,24 +16,30 @@ class ActionTest(Action):
         return []
 
 
-    
 class ActionQuestion(Action):
     def name(self):
         return "action_question"
 
     def run(self, dispatcher, tracker, domain):
+        api = 'https://ludum-duvidas.herokuapp.com/api/duvidas'
         try:
-            dispatcher.utter_message('"Espere jovem padawan, vou procurar uma resposta no StackOverflow para você"')
+            dispatcher.utter_message('Espere jovem padawan,' +
+                                     'vou procurar uma resposta' +
+                                     'no StackOverflow para você')
         except ValueError:
             dispatcher.utter_message(ValueError)
         try:
-            link = requests.get('https://ludum-duvidas.herokuapp.com/api/duvidas')
+            link = requests.get(api)
             stack = json.loads(link.text)
             utterString = ''
-            for i in range (0, len (stack['data'][0]['answer'])):
-                utterString+= 'Resposta ' + str(i + 1) + '\n'
-                utterString+= 'Titulo: ' + str(stack['data'][0]['answer'][i]['title']) + '\n'
-                utterString+= 'Link: '  + str(stack['data'][0]['answer'][i]['link']) + '\n'
+            for i in range(0, len(stack['data'][0]['answer'])):
+                utterString += 'Resposta ' + str(i + 1) + '\n'
+                utterString += 'Titulo: '
+                utterString += (str(stack['data'][0]['answer'][i]['title']))
+                utterString += '\n'
+                utterString += 'Link: '
+                utterString += str(stack['data'][0]['answer'][i]['link'])
+                utterString += '\n'
             dispatcher.utter_message(utterString)
         except ValueError:
             dispatcher.utter_message(ValueError)
