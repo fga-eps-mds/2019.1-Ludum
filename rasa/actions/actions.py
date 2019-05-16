f6b7f16d5bd3174b3aa5b8483a08d9b266cb6602
jonjon667 <jonjon667@hotmail.com>
2019-05-16 20:55:34 -0300

    #57 - Limpando slots após a execução da pesquisa

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -9,26 +9,8 @@ class ActionTest(Action):
         return "action_test"
 
     def run(self, dispatcher, tracker, domain):
-        api = 'https://ludum-duvidas.herokuapp.com/api/duvidas'
         try:
-            dispatcher.utter_message('Espere jovem padawan,' +
-                                     'vou procurar uma resposta' +
-                                     'no StackOverflow para você')
-        except ValueError:
-            dispatcher.utter_message(ValueError)
-        try:
-            link = requests.get(api)
-            stack = json.loads(link.text)
-            utterString = ''
-            for i in range(0, len(stack['data'][0]['answer'])):
-                utterString += 'Resposta ' + str(i + 1) + '\n'
-                utterString += 'Titulo: '
-                utterString += (str(stack['data'][0]['answer'][i]['title']))
-                utterString += '\n'
-                utterString += 'Link: '
-                utterString += str(stack['data'][0]['answer'][i]['link'])
-                utterString += '\n'
-            dispatcher.utter_message(utterString)
+            dispatcher.utter_message('Mensagem enviada por uma custom action')
         except ValueError:
             dispatcher.utter_message(ValueError)
         return []
@@ -70,4 +52,4 @@ class ActionQuestion(Action):
             dispatcher.utter_message(utterString)
         except ValueError:
             dispatcher.utter_message(ValueError)
-        return []
+        return [SlotSet('pergunta', None)]
